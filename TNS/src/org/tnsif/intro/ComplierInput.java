2256097135a15f6eb627be0a000aea6a727c8a3b
heman <heman@LAPTOP-GNFQEBSG>
2023-07-20 14:07:30 +0530

    first commit

---
+++ TNS/src/org/tnsif/intro/ComplierInput.java
@@ -0,0 +1,14 @@
+//program to demonstrate on compile-time-input
+package org.tnsif.intro;
+
+public class ComplierInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short num=34;
+		System.out.println("the num is:"+num);
+		
+
+	}
+
+}
---
+++ TNS/src/org/tnsif/intro/UserInput.java
@@ -0,0 +1,18 @@
+package org.tnsif.intro;
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// user input
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the string");	
+		//String str1=s.next();
+		String str1=s.nextLine();
+		System.out.println("string is:"+str1);
+		
+		
+
+	}
+
+}
